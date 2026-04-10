d181a03d805b58dcd2d74bab676ca365689f3f5c
Souliman Terrah <souliman.terrah@hotmail.com>
2026-04-10 03:59:58 +0100

    fix: update link path in ForgotPassword page and add required attribute to password input in PasswordLoginForm

--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -6,7 +6,7 @@ export default function ForgotPassword() {
     <AuthLogin>
       <ForgotPasswordForm />
       <p>
-        <Link href="/login/identifier" className="text-[#0041d9]">
+        <Link href="/login/password" className="text-[#0041d9]">
           <strong className="text-sm">Back to sign in</strong>
         </Link>
       </p>
