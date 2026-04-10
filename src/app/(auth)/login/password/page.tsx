066b13b65fe5e621d49f4fcd5521625af0f96438
Souliman Terrah <souliman.terrah@hotmail.com>
2026-04-10 03:47:32 +0100

    add PasswordLoginForm.tsx and page.tsx for password authentication

---
+++ src/app/(auth)/login/password/page.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import PasswordLoginForm from './PasswordLoginForm';
+import AuthLogin from '@/components/layout/AuthLogin';
+
+export default function Password() {
+  return (
+    <AuthLogin>
+      <PasswordLoginForm />
+      <div className="flex max-[244px]:flex-col sm:flex-row text-sm">
+        <p>New to Zillow?</p>
+        <Link href="/register" className="text-[#0041d9] ml-2">
+          <strong>Create account</strong>
+        </Link>
+      </div>
+      <div className="flex items-center my-5">
+        <div className="border-b w-full border-[#535364]"></div>
+      </div>
+      <div>
+        <Link href="/send-code-to-email">
+          <strong className="text-[#0041d9] text-sm">Or send a secure code to your email</strong>
+        </Link>
+      </div>
+    </AuthLogin>
+  );
+}
