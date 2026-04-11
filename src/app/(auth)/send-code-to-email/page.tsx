29e8eb1187510a6bb8f2bbdfd98cd59833ceae46
Souliman Terrah <souliman.terrah@hotmail.com>
2026-04-11 04:29:03 +0100

    add: implement SendCodeToEmailForm feature and corresponding page for email verification

---
+++ src/app/(auth)/send-code-to-email/page.tsx
@@ -0,0 +1,26 @@
+import AuthLogin from '@/components/layout/AuthLogin';
+import SendCodeToEmailForm from './SendCodeToEmailForm';
+import Link from 'next/link';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Check your email',
+};
+
+export default function SendCodeToEmail() {
+  return (
+    <AuthLogin>
+      <SendCodeToEmailForm />
+      <div className="mt-15">
+        <div className="flex items-center my-5">
+          <div className="border-b w-full border-[#535364]"></div>
+        </div>
+        <div>
+          <Link href="/login/password">
+            <strong className="text-[#0041d9] text-sm">Or enter a password</strong>
+          </Link>
+        </div>
+      </div>
+    </AuthLogin>
+  );
+}
