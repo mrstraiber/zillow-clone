64626ead131a11acc49c50f6d6b205bf0f454f7c
Souliman Terrah <souliman.terrah@hotmail.com>
2026-04-22 02:22:12 +0100

    feat: implement PasswordRegisterForm and related components for password creation

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -5,7 +5,7 @@ interface ButtonProps {
 export default function Button({ children, isDisabled = false }: ButtonProps) {
   return (
     <button
-      className="w-full rounded-lg bg-[#0041d9] py-3 px-4 text-white font-black cursor-pointer disabled:cursor-not-allowed disabled:opacity-50"
+      className="w-full rounded-xl bg-blue-custom py-3 px-4 text-white font-black cursor-pointer disabled:cursor-not-allowed disabled:opacity-50"
       disabled={isDisabled}
     >
       {children}
